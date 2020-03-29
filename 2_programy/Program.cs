720db78dea44fd7883ed14d8d2e3a56b5bf04507
karmazynowykonterfekt <kasia_michalak@op.pl>
2020-03-29 19:20:50 +0200

    Add project files.

---
+++ 2_programy/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_programy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WYŚWIETL W KONSOLI PROSTOKĄT O SZEROKOŚCI X ORAZ WYSOKOŚCI Y
+            //X ORAZ Y PODAJE UŻYTKOWNIK, PROSTOKĄT ZBUDOWANY JEST Z *
+            Console.Write("Podaj wymiar X: ");
+            string sX = Console.ReadLine();
+           
+            Console.Write("Podaj wymiar Y: ");
+            string sY = Console.ReadLine();
+
+            int[,] wym = new int[Convert.ToInt32(sX), Convert.ToInt32(sY)];
+
+            for (int i = 0; i < wymX.Length; i++)
+                for (int j = 0; j < wymX.Length; j++)
+                Console.Write(wym[i,j].ToString());
+            Console.WriteLine();        
+           
+            Console.ReadKey();
+
+
+        }
+    }
+}
